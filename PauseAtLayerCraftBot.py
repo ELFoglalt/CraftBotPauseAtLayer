955f74e43a50a9a0f299a5c64ee89c743c35c39e
Máté Kerekes <everydaymadnes@gmail.com>
2018-10-02 14:44:19 +0200

    Initial commit

---
+++ PauseAtLayerCraftBot.py
@@ -0,0 +1,96 @@
+from ..Script import Script
+from UM.Logger import Logger
+import re
+
+class PauseAtLayerCraftBot(Script):
+    def __init__(self):
+        super().__init__()
+
+    # This function defines the plugin's appearance (i.e. name), and it's inputs in the Cura UI.
+    # Settings contain the description of fields that show up in the plugin window.
+    def getSettingDataString(self):
+        return """{
+            "name":"Pause at layer (CraftBot)",
+            "description": "Triggers a pause before a specified layer.",
+            "key": "PauseAtHeightCraftBot",
+            "metadata": {},
+            "version": 2,
+            "settings":
+            {
+                "pause_layer":
+                {
+                    "label": "Pause Layer",
+                    "description": "The printer will pause before this layer is printed. This layer includes supports, and corresponds to the layers seen in the Cura viewer. Layer 1 is the first layer of the print.",
+                    "type": "int",
+                    "default_value": 1,
+                    "minimum_value": 1
+                },
+                "message":
+                {
+                    "label": "Pause Message",
+                    "description": "A message to display on the printer. The the string may contain python formatting literals for displaying the layer number.",
+                    "type": "str",
+                    "default_value": "User defined pause at layer {}"
+                },
+                "should_beep":
+                {
+                    "label": "Beep",
+                    "description": "When ticked, the printer will emit a short tone when pausing.",
+                    "type": "bool",
+                    "default_value": true
+                }
+            }
+        }"""
+
+    # This function runs when the actual post processing takes place.
+    #
+    # Data consits of blocks, each containing a multi-line string of gcode.
+    # Cura seems to write things as:
+    #   Block 0: Start comments
+    #   Block 1: Setup code
+    #   Block 2: First layer of print
+    #   Block 3: Second layer of pri nt
+    #   ...
+    #   Block N-1: ?
+    #   Block N: Ending code
+    #
+    def execute(self, data):
+        # Get the values inputted trough the cura UI
+        pause_layer = self.getSettingValueByKey("pause_layer")
+        message = self.getSettingValueByKey("message").format(pause_layer)
+        should_beep = self.getSettingValueByKey("should_beep")
+        beep_duration = 200 # 100 = 1 sec
+        beep_tone = 100
+
+        # Construct gcode to insert
+        gcode_block = [";TYPE:CUSTOM", ";pause added by post processing", ";script: PauseAtLayerCraftBot.py"]
+        if should_beep:
+            gcode_block.append("M300 P200 S100 ;beep")
+        gcode_block.append(self.putValue(G=197) + " " + message + " ;pause")
+        # gcode_block.append(self.putValue(G=198) + " ;resume print")
+
+        true_layer_cntr = 0
+
+        # We iterate over each block, keeping track of the number of ;LAYER## comments, to find
+        # the layer before which we will insert the gcode.
+        for block_idx, block in enumerate(data):
+            
+            lines = block.strip().split("\n")
+            
+            for line_idx, line in enumerate(lines):
+            
+                if ";LAYER:" in line:
+                    true_layer_cntr += 1
+
+                if (true_layer_cntr == pause_layer) and not line.startswith(";"):
+                    new_lines = lines[:line_idx]
+                    new_lines.extend(gcode_block)
+                    new_lines.extend(lines[line_idx:])
+
+                    new_block = "\n".join(new_lines) + "\n"
+
+                    data[block_idx] = new_block
+                    
+                    return data
+
+        return data
---
+++ readme.md
@@ -0,0 +1,7 @@
+# CraftBotPauseAtLayer
+
+> A simple cura PostProcessingPlugin script that adds pause at layer capability for Craft Bot 2 printers, which seem to ignore the pasue command given by the similar script shipped with Ultimaker Cura 3.
+
+## Installation
+
+Copy the `PauseAtLayerCraftBot.py` script into `CURA_CONF_FOLDER\scripts`, and then restart Cura. You can locate your configuration folder trough the Cura menu, via `Help > Show Configuration Folder`.
